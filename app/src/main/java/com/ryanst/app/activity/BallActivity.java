33231d5fa723a509b947bf28572e862cf080f5bf
Ryanst <zhengjuntong@gmail.com>
2016-12-28 11:22:43 +0800

    ball success

--- app/src/main/java/com/ryanst/app/activity/BallActivity.java
+++ app/src/main/java/com/ryanst/app/activity/BallActivity.java
@@ -1,99 +1,132 @@
 package com.ryanst.app.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.rlib.util.AndroidScreenUtil;
 import com.ryanst.app.R;
 import com.ryanst.app.core.BaseActivity;
 
+import android.view.GestureDetector.SimpleOnGestureListener;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnTouch;
 
 /**
  * Created by zhengjuntong on 12/27/16.
  */
 
 public class BallActivity extends BaseActivity implements View.OnTouchListener {
-    @BindView(R.id.activities)
-    ImageView activities;
-    @BindView(R.id.fl_content)
-    RelativeLayout flContent;
     @BindView(R.id.btn_test)
     Button btnTest;
-    private int xDelta;
-    private int yDelta;
-    private boolean click = false;
+    @BindView(R.id.activities)
+    Button ivBall;
+    @BindView(R.id.root)
+    FrameLayout flRoot;
+    private int _xDelta;
+    private int _yDelta;
+    private int screenWidth;
+    private int screenHeight;
+    private int maxMarginLeft;
+    private int maxMarginTop;
+    private int downX;
+    private int downY;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ball);
         ButterKnife.bind(this);
+
+        int size = AndroidScreenUtil.dip2px(this, 70);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+                size, size);
+
+        screenWidth = AndroidScreenUtil.getScreenMetrics(this).x;
+        screenHeight = AndroidScreenUtil.getScreenMetrics(this).y;
+
+        maxMarginLeft = screenWidth - size;
+        layoutParams.leftMargin = maxMarginLeft - AndroidScreenUtil.dip2px(this, 30);
+        maxMarginTop = screenHeight - size - AndroidScreenUtil.dip2px(this, 25);
+        layoutParams.topMargin = maxMarginTop - AndroidScreenUtil.dip2px(this, 30);
+        layoutParams.bottomMargin = 0;
+        layoutParams.rightMargin = 0;
+
+        ivBall.setLayoutParams(layoutParams);
+        ivBall.setOnTouchListener(this);
+
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                click = !click;
-                Toast.makeText(BallActivity.this, "Hello: " + click, Toast.LENGTH_SHORT).show();
-                if (click) {
-                    activities.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Toast.makeText(BallActivity.this, "Click", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                } else {
-                    activities.setOnClickListener(null);
-                }
+                Toast.makeText(BallActivity.this, "Hello", Toast.LENGTH_SHORT).show();
             }
         });
-
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                150, 50);
-        layoutParams.leftMargin = 50;
-        layoutParams.topMargin = 50;
-        layoutParams.bottomMargin = -250;
-        layoutParams.rightMargin = -250;
-        activities.setLayoutParams(layoutParams);
     }
 
+    public boolean onTouch(View view, MotionEvent event) {
+        dealOnTouch(view, event);
+        return true;
+    }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        int X = (int) event.getRawX();
-        int Y = (int) event.getRawY();
-
+    private void dealOnTouch(View view, MotionEvent event) {
+        final int touchX = (int) event.getRawX();
+        final int touchY = (int) event.getRawY();
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
-                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) activities
+                downX = touchX;
+                downY = touchY;
+                FrameLayout.LayoutParams lParams = (FrameLayout.LayoutParams) view
                         .getLayoutParams();
-                xDelta = X - lParams.leftMargin;
-                yDelta = Y - lParams.topMargin;
+                _xDelta = touchX - lParams.leftMargin;
+                _yDelta = touchY - lParams.topMargin;
                 break;
             case MotionEvent.ACTION_UP:
+                if (downX == touchX && downY == touchY) {
+                    Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
                 break;
             case MotionEvent.ACTION_POINTER_UP:
                 break;
             case MotionEvent.ACTION_MOVE:
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) activities
+                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view
                         .getLayoutParams();
-                layoutParams.leftMargin = X - xDelta;
-                layoutParams.topMargin = Y - yDelta;
-                layoutParams.rightMargin = -250;
-                layoutParams.bottomMargin = -250;
-                activities.setLayoutParams(layoutParams);
+
+                int leftMargin;
+
+                if (touchX - _xDelta <= 0) {
+                    leftMargin = 0;
+                } else if (touchX - _xDelta < maxMarginLeft) {
+                    leftMargin = touchX - _xDelta;
+                } else {
+                    leftMargin = maxMarginLeft;
+                }
+
+                layoutParams.leftMargin = leftMargin;
+
+                int topMargin;
+
+                if (touchY - _yDelta <= 0) {
+                    topMargin = 0;
+                } else if (touchY - _yDelta < maxMarginTop) {
+                    topMargin = touchY - _yDelta;
+                } else {
+                    topMargin = maxMarginTop;
+                }
+
+                layoutParams.topMargin = topMargin;
+
+                layoutParams.rightMargin = 0;
+                layoutParams.bottomMargin = 0;
+                view.setLayoutParams(layoutParams);
                 break;
         }
-        flContent.invalidate();
-        return true;
+        flRoot.invalidate();
     }
 }
